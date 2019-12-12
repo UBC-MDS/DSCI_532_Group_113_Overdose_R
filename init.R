877f748e874fcaf8f06f079dca02043034668e1e
HanyingZhang <35021819+HanyingZhang@users.noreply.github.com>
2019-12-12 15:20:44 -0800

    Update init.R

--- init.R
+++ init.R
@@ -53,3 +53,8 @@ remotes::install_github("plotly/dash-table", ref="042ad65")
 remotes::install_github("plotly/dash-html-components", ref="17da1f4")
 remotes::install_github("plotly/dash-core-components", ref="cc1e654")
 remotes::install_github("plotly/dashR", ref="dev", dependencies=FALSE)
+# Packages we used start here!
+remotes::install_github("plotly/dash-table", dependencies=FALSE)
+install.packages("tidyverse")
+install.packages("viridis")
+install.packages("readxl")
